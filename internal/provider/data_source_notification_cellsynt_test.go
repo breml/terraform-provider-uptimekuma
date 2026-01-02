c9ee6331d54fcd7395fff2110ea2dc3da88516ad
Lucas Bremgartner <breml@users.noreply.github.com>
2026-01-02 15:51:39 +0100

    feat: Add support for CellSynt SMS notifications
    Add Terraform resource and data source for CellSynt notifications,
    allowing users to configure SMS gateway notifications for monitoring
    alerts. Includes full test coverage, acceptance tests, and documentation.
    
    Closes: #120

---
+++ internal/provider/data_source_notification_cellsynt_test.go
@@ -0,0 +1,81 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccNotificationCellsyntDataSource(t *testing.T) {
+	name := acctest.RandomWithPrefix("NotificationCellsynt")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNotificationCellsyntDataSourceConfig(name),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.uptimekuma_notification_cellsynt.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(name),
+					),
+				},
+			},
+			{
+				Config: testAccNotificationCellsyntDataSourceConfigByID(name),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.uptimekuma_notification_cellsynt.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(name),
+					),
+				},
+			},
+		},
+	})
+}
+
+func testAccNotificationCellsyntDataSourceConfig(name string) string {
+	return providerConfig() + fmt.Sprintf(`
+resource "uptimekuma_notification_cellsynt" "test" {
+  name            = %[1]q
+  is_active       = true
+  login           = "testuser"
+  password        = "testpass123"
+  destination     = "+46701234567"
+  originator      = "TestSender"
+  originator_type = "Alphanumeric"
+  allow_long_sms  = false
+}
+
+data "uptimekuma_notification_cellsynt" "test" {
+  name = uptimekuma_notification_cellsynt.test.name
+}
+`, name)
+}
+
+func testAccNotificationCellsyntDataSourceConfigByID(name string) string {
+	return providerConfig() + fmt.Sprintf(`
+resource "uptimekuma_notification_cellsynt" "test" {
+  name            = %[1]q
+  is_active       = true
+  login           = "testuser"
+  password        = "testpass123"
+  destination     = "+46701234567"
+  originator      = "TestSender"
+  originator_type = "Alphanumeric"
+  allow_long_sms  = false
+}
+
+data "uptimekuma_notification_cellsynt" "test" {
+  id = uptimekuma_notification_cellsynt.test.id
+}
+`, name)
+}
